7bd1a6f8bd93fb32dfaf9adb0ead22791675588b
sspin <826015751@qq.com>
2021-06-09 16:08:11 +0800

    chore: add Access-Control-Allow-Origin

--- src/index.ts
+++ src/index.ts
@@ -32,6 +32,14 @@ const timeLimitCheck = async (req: any, res: any) => {
     res.send({ ErrorCode: 6, message: 'Format error' });
   }
 };
+app.all('*', function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'X-Requested-With');
+  res.header('Access-Control-Allow-Methods', 'PUT,POST,GET,DELETE,OPTIONS');
+  res.header('X-Powered-By', ' 3.2.1');
+  res.header('Content-Type', 'application/json;charset=utf-8');
+  next();
+});
 
 app.use('/send', timeLimitCheck);
 app.listen(port, localhost, function () {
